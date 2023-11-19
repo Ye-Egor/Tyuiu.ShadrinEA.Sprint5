55e2411dc658f715a00969273030ae1d5a972d53
Ye-Egor <Shadrin-yegor29@yandex.ru>
2023-11-19 18:43:54 +0500

    Task3

---
+++ Tyuiu.ShadrinEA.Sprint5.Task3.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShadrinEA.Sprint5.Task3.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShadrinEA.Sprint5.Task3.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9778e8b9-eada-496a-bdd0-f43c22f893b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShadrinEA.Sprint5.Task3.V1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.ShadrinEA.Sprint5.Task3.V1.Lib;
+
+namespace Tyuiu.ShadrinEA.Sprint5.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шадрин Е.А | ИСТНб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Задание #3                                                                   *");
+            Console.WriteLine("* Вариант #1                                                                   *");
+            Console.WriteLine("* Выполнил: Шадрин Егор Андреевич | ИСТНб-23-1                                 *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в       *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль.                      *");
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("* y(x)=(x^3-8)/(2x^2)                                                          *");
+            Console.WriteLine("********************************************************************************");
+
+
+
+            int x = 3;
+            Console.WriteLine("x = " + x);
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+
+            Console.WriteLine("Фаил: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+        }
+    }
+}
