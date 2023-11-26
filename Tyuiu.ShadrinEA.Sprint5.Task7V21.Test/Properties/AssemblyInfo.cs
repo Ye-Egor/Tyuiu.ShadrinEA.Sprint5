c3e68835bdc91934f08f019bdf156cb6527d7ce9
Ye-Egor <Shadrin-yegor29@yandex.ru>
2023-11-26 19:06:57 +0500

    Task7

---
+++ Tyuiu.ShadrinEA.Sprint5.Task7V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShadrinEA.Sprint5.Task7V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShadrinEA.Sprint5.Task7V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f2c8775e-14a4-469e-95f1-aeb374b28274")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShadrinEA.Sprint5.Task7V21.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.ShadrinEA.Sprint5.Task7V21.Lib;
+
+namespace Tyuiu.ShadrinEA.Sprint5.Task7V21.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\meteo\source\repos\Tyuiu.ShadrinEA.Sprint5\DataSprint5\InPutDataFileTask7V21.txt";
+            FileInfo fi = new FileInfo(path);
+            bool fileExists = fi.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
